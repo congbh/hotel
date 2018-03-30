59da1132ede4bf19daea01f48f17cbe07829b3a8
Bach Hung Cong <bachhungcong@gmail.com>
2018-03-30 15:21:24 +0700

    Add nodemon.json

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,12 +1,3 @@
-const env = process.env.NODE_ENV || 'development'
+const env = process.env.NODE_ENV
 const config = require('../../knexfile')[env]
 module.exports = require('knex')(config)
-
-// module.exports.TABLES = {
-//   USER: 'users',
-//   HOTEL: 'hotel',
-//   FLOOR: 'floor',
-//   ROOM: 'room',
-//   SERVICE: 'service',
-//   CATALOG: 'service_catalog'
-// }
