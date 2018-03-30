4a40dd0ac6b6e6278a598b72be216b1843c78591
Bach Hung Cong <bachhungcong@gmail.com>
2018-03-30 15:34:05 +0700

    Update

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,16 @@ require('dotenv').config({silent: true})
 const path = require('path')
 
 module.exports = {
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || '',
+    migrations: {
+      directory: path.join(__dirname, 'src', 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'src', 'db', 'seeds')
+    }
+  },
   development: {
     client: 'pg',
     connection: process.env.DATABASE_URL || '',
